83484010c7c6c4193e37957efa596bdf255020ea
Thoq <tristanshaw000@gmail.com>
2026-03-22 22:14:40 -0400

    change launcher jdk, add arch build support

---
+++ scripts/install-arch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+DEB=$(find src-tauri/target/release/bundle/deb -name "*.deb" | head -1)
+
+if [ -z "$DEB" ]; then
+  echo "no deb found, running tauri build..."
+  cargo tauri build --bundles deb
+  DEB=$(find src-tauri/target/release/bundle/deb -name "*.deb" | head -1)
+fi
+
+printf "Alya Client\nGPL-3\nNYA\n" | debtap "$DEB"
+
+PKG=$(find src-tauri/target/release/bundle/deb -name "*.pkg.tar.zst" | head -1)
+
+sudo pacman -U --nodeps --nodeps "$PKG"
